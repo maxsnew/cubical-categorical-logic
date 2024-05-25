e0dfb69a6237c6b323871db154eee8bfbb80c986
Steven Schaefer <stschaef@umich.edu>
2024-05-25 02:41:58 -0400

    line length script update (#99)
    * update gitignore + line length script update
    
    * get rid of shopt globstar

--- check-line-lengths.sh
+++ check-line-lengths.sh
@@ -2,8 +2,31 @@
 # originally written by Steven Schaefer <stschaef>
 
 error=0
-shopt -s globstar nullglob
-for file in **/*.agda; do
+
+VENV_DIR="env"
+
+# Check if the virtual environment directory exists
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Virtual environment does not exist. Creating one now..."
+    # Create the virtual environment with python3
+    python3 -m venv "$VENV_DIR" || { echo "Failed to create virtual environment."; exit 1; }
+
+fi
+
+source "$VENV_DIR/bin/activate"
+
+if [ $? -ne 0 ]; then
+    echo "Error: Unable to activate the virtual environment."
+    exit 2
+fi
+
+echo "Virtual environment activated."
+
+pip3 install wcwidth || { echo "Failed to install wcwidth."; exit 3; }
+
+echo "Package 'wcwidth' has been successfully installed in the virtual environment."
+
+while IFS= read -r -d '' file; do
   python3 -c '
 import sys
 from wcwidth import wcswidth
@@ -18,5 +41,5 @@ except Exception as e:
     print(f"Error processing {filename}: {e}")
     sys.exit(1)
   ' "${file}" || error=1
-done
+done  < <(find . -name '*.agda' -print0)
 exit ${error}
